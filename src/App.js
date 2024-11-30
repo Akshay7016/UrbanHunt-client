c871148013ca88ca0d7a611bfcb207dc0f301e81
Akshay7016 <asajgule@gmail.com>
2024-11-30 22:29:30 +0530

    HomePage created

--- src/App.js
+++ src/App.js
@@ -1,9 +1,17 @@
 import { Navbar } from "components/Navbar/Navbar";
+import { HomePage } from "pages/HomePage/HomePage";
 
 import "./layout.scss";
 
 export const App = () => {
   return (
-    <div className="layout"><Navbar /></div>
+    <div className="layout">
+      <div className="navbar">
+        <Navbar />
+      </div>
+      <div className="content">
+        <HomePage />
+      </div>
+    </div>
   )
 }
