3002dad5fd4bec57c137be2c7c49d8ae1e90ba8f
Akshay7016 <asajgule@gmail.com>
2024-12-14 19:58:20 +0530

    Map pin icon issue fixed

--- src/components/Pin/Pin.js
+++ src/components/Pin/Pin.js
@@ -1,7 +1,17 @@
 import { Marker, Popup } from 'react-leaflet';
 import { Link } from 'react-router-dom';
+import L from 'leaflet';
+import iconMarker from 'leaflet/dist/images/marker-icon.png';
+import iconRetina from 'leaflet/dist/images/marker-icon-2x.png';
+import iconShadow from 'leaflet/dist/images/marker-shadow.png';
 
 import './Pin.scss';
+
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: iconRetina,
+  iconUrl: iconMarker,
+  shadowUrl: iconShadow,
+});
 
 export const Pin = ({
   item: { id, title, images, bedroom, price, latitude, longitude },
