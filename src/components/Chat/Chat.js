23125a69283f3a578281ebfa897d519d73f41719
Akshay7016 <asajgule@gmail.com>
2024-12-17 17:17:48 +0530

    height on Chats section updated for md and sm screens

--- src/components/Chat/Chat.js
+++ src/components/Chat/Chat.js
@@ -14,7 +14,7 @@ export const Chat = () => {
         <div className="messages">
           <h1>Messages</h1>
           {[1, 2, 3, 4, 5].map((item) => (
-            <div className="message" onClick={() => setChat(item)}>
+            <div className="message" key={item} onClick={() => setChat(item)}>
               <img src={img} alt="user-image" />
               <span>{name}</span>
               <p>Some message.....</p>
@@ -33,6 +33,90 @@ export const Chat = () => {
             </span>
           </div>
           <div className="center">
+            <div className="chatMessage">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>How are you</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>I am fine</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>How are you?</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>I am doing good</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem Lorem Lorem Lorem</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>How are you</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>I am fine</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>How are you?</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>I am doing good</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem Lorem Lorem Lorem</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>How are you</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>Hi</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>I am fine</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage own">
+              <p>How are you?</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>I am doing good</p>
+              <span>1 hour ago</span>
+            </div>
+            <div className="chatMessage">
+              <p>Lorem Lorem Lorem Lorem</p>
+              <span>1 hour ago</span>
+            </div>
             <div className="chatMessage">
               <p>Hi</p>
               <span>1 hour ago</span>
