66faa3ddd7299aa11ab292cfa44de64ce11f4699
Akshay7016 <asajgule@gmail.com>
2024-12-24 13:31:54 +0530

    Login and Logout functionality implemented

--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,5 +1,72 @@
+import { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link, useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
+import { apiRequest } from 'lib/apiRequest';
+
 import './Login.scss';
 
 export const Login = () => {
-  return <div>Login</div>;
+  const navigate = useNavigate();
+  const [isLoading, setIsLoading] = useState(false);
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+    reset,
+  } = useForm();
+
+  const submitHandler = async (data) => {
+    try {
+      setIsLoading(true);
+      const { data: userData } = await apiRequest.post('/auth/login', data);
+      localStorage.setItem('user', JSON.stringify(userData));
+      reset();
+      navigate('/');
+    } catch (error) {
+      toast.error(error?.response?.data?.message);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div className="loginPage">
+      <div className="formContainer">
+        <form onSubmit={handleSubmit(submitHandler)} noValidate>
+          <h1>Welcome back</h1>
+          <div className="inputFieldContainer">
+            <input
+              type="email"
+              {...register('email', {
+                required: 'Email is required',
+              })}
+              placeholder="Email"
+            />
+            {errors?.email?.message && <span>{errors?.email?.message}</span>}
+          </div>
+
+          <div className="inputFieldContainer">
+            <input
+              type="password"
+              {...register('password', {
+                required: 'Password is required',
+              })}
+              placeholder="Password"
+            />
+            {errors?.password?.message && (
+              <span>{errors?.password?.message}</span>
+            )}
+          </div>
+
+          <button disabled={isLoading}>Login</button>
+          <Link to="/register">Don't you have an account?</Link>
+        </form>
+      </div>
+      <div className="imageConatiner">
+        <img src="/images/bg.png" alt="bg-img" />
+      </div>
+    </div>
+  );
 };
--- src/pages/ProfilePage/ProfilePage.js
+++ src/pages/ProfilePage/ProfilePage.js
@@ -1,10 +1,26 @@
+import { useNavigate } from 'react-router-dom';
+import toast from 'react-hot-toast';
+
 import { userData } from 'lib/dummyData';
 import { List } from 'components/List/List';
 import { Chat } from 'components/Chat/Chat';
+import { apiRequest } from 'lib/apiRequest';
 
 import './ProfilePage.scss';
 
 export const ProfilePage = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    try {
+      await apiRequest.post('/auth/logout');
+      localStorage.removeItem('user');
+      navigate('/');
+    } catch (error) {
+      toast.toast.error(error?.response?.data?.message);
+    }
+  };
+
   return (
     <div className="profilePage">
       <div className="details">
@@ -23,6 +39,7 @@ export const ProfilePage = () => {
             <span>
               E-mail: <b>akshay@test.com</b>
             </span>
+            <button onClick={handleLogout}>Logout</button>
           </div>
           <div className="title">
             <h1>My List</h1>
